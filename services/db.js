52c2e27020a0275320a4c9ec0eb97a388bbcdd5d
Michael Akinwonmi <akinwonmim@gmail.com>
2025-12-27 14:43:45 +0100

    Update db.js

--- services/db.js
+++ services/db.js
@@ -7,7 +7,7 @@ const client = new Client({
   host: process.env.DB_HOST || "localhost",
   port: process.env.DB_PORT || 5432,
   user: process.env.DB_USER || "postgres",
-  password: process.env.DB_PASSWORD || "yourPassword",
+  password: process.env.DB_PASSWORD || "postgres-password",
   database: process.env.DB_NAME || "interestAPI",
 });
 
